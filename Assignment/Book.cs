92dd1213e5ca6a3f95e8da0a29095f09c303391a
malakelsayyad <malakelsayyad@yahoo.com>
2025-01-30 21:56:15 +0200

    Assignment

---
+++ Assignment/Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN : {ISBN} , Title : {Title} , Authors : {Authors} , Publication Date : {PublicationDate} , Price : {Price}";
+        }
+    }
+
+    public delegate string BookDelegate(Book book);
+    public class BookFunctions 
+    {
+        public static string GetTitle(Book B)
+        {
+            return B?.Title ?? "Unkown Title";
+        }
+
+        public static string GetAuthors(Book B) 
+        {
+            if (B?.Authors == null || B.Authors.Length == 0)
+                return "No Authors";
+            else
+               return string.Join(", ", B.Authors); 
+        }
+
+        public static decimal GetPrice(Book B)
+        {
+            return B?.Price??0;
+        }
+
+    }
+
+    public class LibraryEngine 
+    {
+        public static void ProcessBooks( List<Book> bList ,BookDelegate bookDelegate)
+        {
+            foreach (Book book in bList) 
+            {   
+                Console.WriteLine(bookDelegate(book));
+            }
+        }
+
+        public static void ProcessBooks(List<Book> bList, Func<Book, string> processBook)
+        {
+            foreach (Book book in bList)
+            {
+                Console.WriteLine(processBook(book));
+            }
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01 Question01
+            //Book book1 = new Book("1", "C# Programming", new string[] { "John Doe", "Jane Smith" }, new DateTime(2023, 10, 1), 29m);
+            //Book book2 = new Book("2", "Learning C#", new string[] { "Alice Johnson" }, new DateTime(2022, 5, 15), 19.99m);
+            //Book book3 = new Book("3", "Advanced C#", new string[] { "Bob Brown", "Charlie Davis" }, new DateTime(2021, 8, 20), 39.99m);
+            //Book book4 = new Book("4", "C# for Beginners", new string[] { "Eve White" }, new DateTime(2020, 3, 10), 14.99m);
+
+            //string title =BookFunctions.GetTitle(book1);
+            //Console.WriteLine($"Title  : {title}");
+
+            //string authors = BookFunctions.GetAuthors(book1);
+            //Console.WriteLine($"Authors of {book1.Title} : {authors}");
+
+            //decimal price = BookFunctions.GetPrice(book1);
+            //Console.WriteLine($"Price  {price}"); 
+            #endregion
+
+            #region Second Question
+
+            //Book[] books = new Book[] 
+            //{
+            //  new Book("1", "C# Programming", new string[] { "John Doe", "Jane Smith" }, new DateTime(2023, 10, 1), 29m),
+            //  new Book("2", "Learning C#", new string[] { "Alice Johnson" }, new DateTime(2022, 5, 15), 19.99m)
+
+            //};
+
+            #region User-Defined Delegate
+            //BookDelegate bookDelegate = BookFunctions.GetTitle;
+            //Console.WriteLine("Titles : ");
+            //LibraryEngine.ProcessBooks(books.ToList(), bookDelegate); 
+            #endregion
+
+            #region Func Delegate
+            //Func<Book, string> titleFunc = BookFunctions.GetTitle;
+            //Console.WriteLine("Titles : ");
+            //LibraryEngine.ProcessBooks(books.ToList(),titleFunc); 
+            #endregion
+
+            #region Anonymous
+            //BookDelegate anonymous = delegate (Book b) { return b?.ISBN ?? "Unkown"; };
+            //Console.WriteLine("ISBNS : ");
+            //LibraryEngine.ProcessBooks(books.ToList(),anonymous); 
+            #endregion
+
+            #region Lambda Expression
+            //BookDelegate lambdaExpression = B => B?.PublicationDate.ToShortDateString() ?? "Unkown";
+            //Console.WriteLine("Publication dates : ");
+            //LibraryEngine.ProcessBooks(books.ToList(),lambdaExpression); 
+            #endregion
+
+            #endregion
+        }
+    }
+}
